282c6894665d9ad248d5fd65154994c1e83acc1b
PKneo <pkneo63@gmail.com>
2023-05-15 11:59:35 +0200

    Clean code

--- global.c
+++ global.c
@@ -3,8 +3,6 @@
 //
 #include "global.h"
 
-//Test de merge i push
-
 // Aquesta funció serveix perquè l'usuari introdueixi un enter, el paràmetre és per imprimir un missatge per l'usuari.
 // retorna
 int entradaInt(char missatge[]) {
